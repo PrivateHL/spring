4af5749719877fd5c9766e74781fb62e8e685288
PrivateHL <PrivateHL@163.com>
2019-10-09 18:46:35 +0800

    ..

--- CLOUD/eurekaclientuserservice/src/main/java/common/App.java
+++ CLOUD/eurekaclientuserservice/src/main/java/common/App.java
@@ -2,9 +2,10 @@ package common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
